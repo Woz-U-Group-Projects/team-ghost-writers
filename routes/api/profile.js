56c2078d44af31ccb286db010b5242298f2240ce
Ben <benjaminjfaul@gmail.com>
2018-12-15 16:58:47 -0600

    created route that checks for users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,14 @@
 //location bio testimony social media links 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load profile model
+const Profile = require('../../models/Profile');
+// Load user profile
+const User = require('../../models/User');
+
 
 //@route  GET api/profile/test
 //@desc   tests profile route
@@ -10,6 +18,23 @@ const router = express.Router();
 //whatever request I want: post/get etc 
 //the beginning of the route is already in server.js 
 router.get('/test', (req, res) => res.json({msg: 'Profile Works'}));
+
+//@route  GET api/profile 
+//@desc   Get current users profile
+//@access Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => releaseEvents.status(404).json(err));
+});
+
 
 //must export to work
 module.exports = router;
