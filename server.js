a18add9f7e6c2d4ac44bbb80b58106686c20184c
SethWickham <seth.wickham@hotmail.com>
2018-12-17 10:43:45 -0700

    initial Express server with route files

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+
+const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("mongodb connected :)"))
+  .catch(err => console.log("mongodb not connected"));
+
+app.get("/", (req, res) => res.send("hello Mr. Saint"));
+
+//use routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Server Running on Port 5000"));
