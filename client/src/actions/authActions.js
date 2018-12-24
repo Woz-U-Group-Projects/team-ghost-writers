abc5ceaa9c601b850d4879b025ff5289b4665656
Benjamin Faul <benjaminjfaul@gmail.com>
2018-12-23 22:42:17 -0600

    Imported Redux to login/register pages

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { TEST_DISPATCH } from './types';
+
+// Register User
+export const registerUser =(userData) => {
+  return {
+    type: TEST_DISPATCH,
+    payload: userData
+  };
+};
