ae06763950271d06f93ade69325d27548e8efdcf
Benjamin Faul <benjaminjfaul@gmail.com>
2018-12-28 04:48:15 -0600

    added deleteAccount

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,8 @@ import {
   GET_PROFILE, 
   PROFILE_LOADING, 
   CLEAR_CURRENT_PROFILE, 
-  GET_ERRORS} from './types';
+  GET_ERRORS,
+  SET_CURRENT_USER} from './types';
 
 //Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -35,6 +36,25 @@ export const createProfile=(profileData, history) => dispatch => {
       payload: err.response.data
     })
   );
+};
+
+// Delete account & profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    axios
+      .delete('/api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+        ).catch(err =>
+          dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+          })
+        );
+  }
 };
 
 // Profile loading
