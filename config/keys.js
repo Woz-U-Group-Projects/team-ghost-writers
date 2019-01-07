accd962797dc2be1baed17b332def14dd570e8a4
SethWickham <seth.wickham@hotmail.com>
2019-01-07 14:11:36 -0800

    prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-//Importing MongoDB
-module.exports = {
-  mongoURI: 'mongodb://benfaul:engage19@ds133776.mlab.com:33776/engage',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
