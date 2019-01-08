cb47741752c7563ff1dc9f63d5ced8d031d1bcb6
Benjamin Faul <benjaminjfaul@gmail.com>
2019-01-08 01:26:58 -0600

    ready for deployment

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 //Passport is Authentication
 const passport = require('passport');
-const path = require('path');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const path = require('path');
 
 const app = express ();
 
@@ -34,14 +34,15 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Serve static assets if in production
+//if none of above hits. hit below for heroku
+// Server static assets if in production
 if(process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+//Set static folder
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 }
 
 //When connecting to Heroku
