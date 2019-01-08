cbdf97883275c5d68d556ba6468e09469598cce4
Benjamin Faul <benjaminjfaul@gmail.com>
2019-01-08 00:35:31 -0600

    added Procfile

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ if(process.env.NODE_ENV === 'production') {
   });
 }
 
+//When connecting to Heroku
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
