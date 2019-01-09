d66341f74cb5341e8635042701cc99b417d4b655
Benjamin Faul <benjaminjfaul@gmail.com>
2019-01-09 01:18:38 -0600

    Procfile

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.get('*', (req, res) => {
 }
 
 //When connecting to Heroku
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
