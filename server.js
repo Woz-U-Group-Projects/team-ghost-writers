cb74cf05a1081a2c6450382237661692a5e16e93
Benjamin Faul <benjaminjfaul@gmail.com>
2019-01-12 01:00:00 -0600

    refresh branch

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-//Passport is Authentication
 const passport = require('passport');
+const path = require('path');
+
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-const path = require('path');
 
-const app = express ();
+const app = express();
 
-// Body Parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-//DB config
+// DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db)
+mongoose
+  .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
-  
-//Passport middleware
+
+// Passport middleware
 app.use(passport.initialize());
 
-//Passport Config
+// Passport Config
 require('./config/passport')(passport);
 
 // Use Routes
@@ -34,19 +34,20 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-//if none of above hits. hit below for heroku
+//if none of above hits. hit below for heroku 
 
-//Server static assets if in production
-if(process.env.NODE_ENV === 'production') {
-//Set static folder
-app.use(express.static('client/build'));
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+//we are pointing to the index.html file 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-//When connecting to Heroku
+//when connecting to heroku
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
